a135166e69c6b75acc1a0b4a03d3e9734d5247a4
Danil <shwedx@Yandex.ru>
2023-01-28 21:14:27 +0300

    First HomeWork

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число >");
+
+string firstnumber = Console.ReadLine();
+
+Console.Write("Введите второе число число >");
+
+string secondnumber = Console.ReadLine();
+
+int First;
+int Second;
+
+First = Convert.ToInt32(firstnumber);
+Second = Convert.ToInt32(secondnumber);
+
+if(First > Second)
+{
+    Console.WriteLine(First + " Большее");
+}
+else
+{
+    Console.WriteLine(Second +" Большее");
+}
